a17f071de8defa98b62e58b3e73e83e54849eff8
Maximiliano <maximilianogalindo7@gmail.com>
2022-06-12 22:09:31 -0500

    initial commit

---
+++ Schwarzschild.C
@@ -0,0 +1,18 @@
+#include "GyotoUtils.h"
+#include "GyotoFactoryMessenger.h"
+#include "GyotoKerrBL.h"
+#include "GyotoWorldline.h"
+#include "GyotoError.h"
+#include "GyotoProperty.h"
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+
+using namespace std ; 
+using namespace Gyoto ;
+using namespace Gyoto::Metric ;
